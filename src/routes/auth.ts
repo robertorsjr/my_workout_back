6e6e202d1756c9b71cecd515ebac1529f01c0bc3
Roberto <beto.rodriguesds@gmail.com>
2023-08-04 19:49:43 -0300

    fix: set await in auth post user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,5 +4,5 @@ import { getUser, postUser } from "../controllers/authController";
 
 export async function authRoutes(fastify: FastifyInstance) {
 	fastify.get('/me', {onRequest: [authenticate]} , getUser)
-	fastify.post('/users', async (request) => postUser(fastify, request))
+	fastify.post('/users', async (request) => await postUser(fastify, request))
 }
